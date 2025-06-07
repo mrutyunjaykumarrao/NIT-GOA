8883d6c92604e160fb12c00b9f23f547d020c4dc
Mrutyunjay Kumar Rao <mrutyunjaykumarrao@Mrutyunjays-MacBook-Air.local>
2025-06-07 15:15:52 +0530

    Added Test HomePage.js and Homepage.css

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDUb7MQRvbsPv61BqAuXW7tvVSnpIoO-rg",
-  authDomain: "nit-goa-28558.firebaseapp.com",
-  projectId: "nit-goa-28558",
-  storageBucket: "nit-goa-28558.firebasestorage.app",
-  messagingSenderId: "64833975558",
-  appId: "1:64833975558:web:6bb452c8953167ff991a87",
-  measurementId: "G-L71MQYVNWF"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
