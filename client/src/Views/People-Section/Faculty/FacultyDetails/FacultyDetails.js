f0e62d1df84fc6c1b83153613bca3d70a6c6ad84
Mrutyunjay Kumar Rao <mrutyunjaykumarrao@gmail.com>
2025-07-20 23:20:21 +0530

    Add faculty details for Dr. Prasenjit Dey, Dr. Pravin Anandrao Pawar, Dr. Samar Singhal, and Dr. B. Santhi; update faculty data structure and images

--- client/src/Views/People-Section/Faculty/FacultyDetails/FacultyDetails.js
+++ client/src/Views/People-Section/Faculty/FacultyDetails/FacultyDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useTheme } from '../../../contexts/ThemeContext';
+import { useTheme } from '../../../../contexts/ThemeContext';
 import './FacultyDetailsNew.css';
 
 // Import detailed faculty data
